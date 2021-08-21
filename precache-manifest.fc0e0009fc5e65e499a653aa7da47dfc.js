05eb7038b6b2cb3a146de09d284492e25b75432c
narayan <narayan3119@gmail.com>
2021-08-21 19:12:57 +0530

    Updates

---
+++ precache-manifest.fc0e0009fc5e65e499a653aa7da47dfc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8934052269c7b2145fcc5277bf438db",
+    "url": "/payment-gateway/index.html"
+  },
+  {
+    "revision": "caa73add3c85c9590b50",
+    "url": "/payment-gateway/static/css/main.44a2bd37.chunk.css"
+  },
+  {
+    "revision": "366eb52f33c2c56f6fa4",
+    "url": "/payment-gateway/static/js/2.332a7c07.chunk.js"
+  },
+  {
+    "revision": "1c09e937ebadfa1c56863abdf57fc053",
+    "url": "/payment-gateway/static/js/2.332a7c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa73add3c85c9590b50",
+    "url": "/payment-gateway/static/js/main.b8285dab.chunk.js"
+  },
+  {
+    "revision": "8acd6b34710ca236dee5",
+    "url": "/payment-gateway/static/js/runtime-main.66f8979d.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/payment-gateway/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/payment-gateway/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/payment-gateway/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/payment-gateway/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/payment-gateway/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/payment-gateway/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/payment-gateway/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "a869e05152884c37179adcf1c0b0895d",
+    "url": "/payment-gateway/static/media/first.a869e051.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/payment-gateway/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/payment-gateway/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/payment-gateway/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "e57919d2f9badc1d4f37e282efb34bfa",
+    "url": "/payment-gateway/static/media/second.e57919d2.png"
+  },
+  {
+    "revision": "f452fafe1ed9c1efe9963c10d32c1027",
+    "url": "/payment-gateway/static/media/third.f452fafe.png"
+  }
+]);
